8f4d84bd7967b3bac162121bec64bdc6d333603e
Chen <2934570984@qq.com>
2023-10-29 22:00:41 +0800

    feat: 构建移动端路由表

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>{{ isMobileTerminal }}</div>
+  <!-- 一级路由出口 -->
+  <router-view/>
 </template>
 <script setup>
 import { isMobileTerminal } from './utils/flexible';
---
+++ src/views/main/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    我是移动端主页
+  </div>
+</template>
+
+<script setup>
+
+</script>
