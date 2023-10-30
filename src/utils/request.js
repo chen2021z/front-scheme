d5bdb1e3bc35749cc38ac952eac12a2d121737f6
Chen <2934570984@qq.com>
2023-10-30 15:46:57 +0800

    feat: 配置axios响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,4 +21,16 @@ service.interceptors.request.use(
   }
 )
 
+
+/**
+ * 响应拦截器，响应数据之后，then之前被调用
+ */
+service.interceptors.response.use((response)=>{
+  const {success, message, data} = response.data
+  if(success){
+    return data
+  }
+
+  return Promise.reject(new Error(message))
+})
 export default service
