7f24931566c47658ff3bb5c176fb098de2d4fc80
Chen <2934570984@qq.com>
2023-10-31 20:27:53 +0800

    feat: 完成header模块处理

---
+++ src/views/layout/components/header/header-my.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    my
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    header
+  <div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
+    <div class="flex items-center">
+      <img
+        src="@/assets/vue.svg"
+        class="h-4 cursor-pointer mr-2"
+        @click="onToHome"
+      />
+      <header-search-vue></header-search-vue>
+      <header-theme-vue></header-theme-vue>
+      <header-my-vue></header-my-vue>
+    </div>
   </div>
 </template>
 
 <script setup>
+import headerMyVue from './header-my.vue'
+import headerThemeVue from './header-theme.vue'
+import headerSearchVue from './header-search/index.vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const onToHome = () => {
+  router.push('/')
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
