9ad9f3780abc5ef061fd60f609a93373151e4d48
Chen <2934570984@qq.com>
2023-10-31 18:41:53 +0800

    feat: 通过vite的glob和defineAsyncComponent实现自动化注册组件

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,17 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
+
 
 export default{
+  // 自动化注册组件
   install(app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popup)
+    // 1. 获取当前路径下所有文件夹的 index.vue
+    const components = import.meta.glob('./*/index.vue')
+    // 2. 遍历过去的组件模块
+    for(let key in components){
+      const componentName = 'm-' + key.replace('./', '').split('/')[0]
+    // 3. 利用 app.component 进行注册 
+      app.component(componentName, defineAsyncComponent(components[key]))
+    }
+
   }
 }
