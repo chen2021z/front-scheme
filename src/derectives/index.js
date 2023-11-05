9587eb15317cb52347ea7324f0169fe518089b87
Chen <2934570984@qq.com>
2023-11-05 14:51:34 +0800

    feat: 更改为 vite 自动注册指令

--- src/derectives/index.js
+++ src/derectives/index.js
@@ -1,10 +1,13 @@
-import lazy from './modules/lazy'
-
 /**
  * 全局注册指令
  */
 export default {
   install(app) {
-    app.directive('lazy', lazy)
+    const directives = import.meta.globEager('./modules/*.js')
+    for (const [key, value ] of Object.entries(directives)){
+      const arr = key.split('/')
+      const directiveName = arr[arr.length - 1].replace('.js', '')
+      app.directive(directiveName, value.default)
+    }
   }
 }
