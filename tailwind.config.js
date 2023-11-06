73c767640a5722dec27934dc84bc7e6e5dfa2774
Chen <2934570984@qq.com>
2023-11-06 22:21:42 +0800

    feat: 初步构建message

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,33 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        xs: ['0.25rem','0.35rem'],
-        sm: ['0.35rem','0.45rem'],
-        base: ['0.45rem','0.55rem'],
-        lg: ['0.55rem','0.65rem'],
-        xl: ['0.65rem','0.75rem'],
+        xs: ['0.25rem', '0.35rem'],
+        sm: ['0.35rem', '0.45rem'],
+        base: ['0.45rem', '0.55rem'],
+        lg: ['0.55rem', '0.65rem'],
+        xl: ['0.65rem', '0.75rem']
       },
       // 自定义样式
       boxShadow: {
-        'l-white':'-10px 0 10px white',
-        'l-zinc':'-10px 0 10px #18181b'
+        'l-white': '-10px 0 10px white',
+        'l-zinc': '-10px 0 10px #18181b'
       },
-      height:{
-        header:'72px',
-        main:'calc(100vh - 72px)'
+      height: {
+        header: '72px',
+        main: 'calc(100vh - 72px)'
       },
-      colors:{
+      colors: {
         main: '#f44c58',
         'hover-main': '#f32836',
+        'success-100': '#F2F9EC',
+        'success-200': '#E4F2DB',
+        'success-300': '#7EC050',
+        'warn-100': '#FCF6ED',
+        'warn-200': '#F8ECDA',
+        'warn-300': '#DCA550',
+        'error-100': '#ED7456',
+        'error-200': '#f3471c',
+        'error-300': '#ffffff'
       }
     }
   },
