fd2a38f4749c7b8f050a9fa69421e377ed467a4f
Chen <2934570984@qq.com>
2023-11-06 16:35:54 +0800

    feat: 完成函数组件 confirm

--- src/libs/button/index.vue
+++ src/libs/button/index.vue
@@ -30,7 +30,8 @@
 
 <script>
 const typeEnum = {
-  primary: 'text-white bg-zinc-800 dark:bg-zinc-900 hover:bg-zinc-900 dark:hover:bg-zinc-700 active:bg-zinc-800 dark:active:bg-zinc-700',
+  primary:
+    'text-white bg-zinc-800 dark:bg-zinc-900 hover:bg-zinc-900 dark:hover:bg-zinc-700 active:bg-zinc-800 dark:active:bg-zinc-700',
   main: 'text-white bg-main dark:bg-zinc-900 hover:bg-hover-main dark:hover:bg-zinc-700 active:main dark:active:bg-zinc-700',
   info: 'text-zinc-800 dark:text-zinc-300 bg-zinc-200 dark:bg-zinc-700 hover:bg-zinc-300 dark:hover:bg-zinc-600 active:bg-zinc-200 dark:active:bg-zinc-700'
 }
@@ -55,9 +56,9 @@ const sizeEnum = {
   }
 }
 const EMITS_CLICK = 'click'
-
 </script>
 <script setup>
+import mSvgIcon from '../svg-icon/index.vue'
 import { computed } from 'vue'
 
 /**
@@ -130,7 +131,6 @@ const onBtnClick = () => {
   }
   emits(EMITS_CLICK)
 }
-
 </script>
 
 <style lang="scss" scoped></style>
--- src/libs/index.js
+++ src/libs/index.js
@@ -1,4 +1,5 @@
 import { defineAsyncComponent } from 'vue'
+export { confirm } from './confirm'
 
 
 export default{
