d28404b001f141a39e907475c0b8495c07eac4d3
Chen <2934570984@qq.com>
2023-11-12 21:18:05 +0800

    fix: 处理 pins 刷新空白问题

--- src/router/modules/mobile-routes.js
+++ src/router/modules/mobile-routes.js
@@ -3,5 +3,11 @@ export default [
     path: '/',
     name: 'home',
     component: () => import('@/views/main/index.vue'),
+    children: []
+  },
+  {
+    path: '/pins/:id',
+    name: 'pins',
+    component: () => import('@/views/pins/index.vue')
   }
 ]
--- src/views/pins/components/pins.vue
+++ src/views/pins/components/pins.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed left-0 top-0 w-screen h-screen z-20  xl:backdrop-blur-3xl xl:bg-transparent bg-white pb-2 overflow-y-auto xl:p-2"
+    class="fixed left-0 top-0 w-screen h-screen z-20 bg-zinc-200 pb-2 overflow-y-auto xl:p-2"
   >
     <!-- 移动端下展示 navbar -->
     <m-navbar v-if="isMobileTerminal" @clickLeft="onPop" @clickRight="onPop" sticky>
@@ -21,8 +21,8 @@
       fillClass="fill-zinc-400"
       @click="onPop"
     ></m-svg-icon>
-
-    <div class="xl:w-[80%] xl:h-full xl:mx-auto xl:rounded-lg xl:flex">
+    <!-- 内容区 -->
+    <div v-if="pexelData.title" class="xl:w-[80%] xl:h-full xl:mx-auto xl:rounded-lg xl:flex">
       <img
         class="w-screen mb-2 xl:w-3/5 xl:h-full xl:rounded-tl-lg xl:rounded-bl-lg"
         :src="pexelData.photo"
