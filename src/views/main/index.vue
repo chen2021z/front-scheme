aa16a148f76e1e5de7a1409d5bab3f6b7c995479
Chen <2934570984@qq.com>
2023-11-17 20:54:34 +0800

    feat: 构建通用组件 trigger-menu和 trigger-menu-item

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="h-full overflow-auto bg-white dark:bg-zinc-800 duration-500 xl:scrollbar-thin xl:scrollbar-thumb-zinc-200 xl:dark:scrollbar-thumb-zinc-900 scrollbar-track-transparent">
+  <div
+    class="h-full overflow-auto bg-white dark:bg-zinc-800 duration-500 xl:scrollbar-thin xl:scrollbar-thumb-zinc-200 xl:dark:scrollbar-thumb-zinc-900 scrollbar-track-transparent"
+  >
     <navigation-vue></navigation-vue>
 
     <div class="max-w-screen-xl mx-auto relative xl:mt-1">
       <list-vue></list-vue>
     </div>
+    <m-trigger-menu
+      v-if="isMobileTerminal"
+      class="fixed bottom-6 m-auto left-0 right-0 w-[220px]"
+    >
+      <m-trigger-menu-item
+        icon="home"
+        iconClass="fill-zinc-900 dark:fill-zinc-200"
+      >
+        首页
+      </m-trigger-menu-item>
+      <m-trigger-menu-item
+        v-if="$store.getters.token"
+        icon="vip"
+        iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onVipClick"
+      >
+        VIP
+      </m-trigger-menu-item>
+      <m-trigger-menu-item
+        icon="profile"
+        iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500"
+        @click="onMyClick"
+      >
+        {{ $store.getters.token ? '我的' : '去登录' }}
+      </m-trigger-menu-item>
+    </m-trigger-menu>
   </div>
 </template>
-
 
 <script setup>
 import navigationVue from './components/navigation/index.vue'
 import listVue from './components/list/index.vue'
+import {isMobileTerminal} from '@/utils/flexible'
 </script>
